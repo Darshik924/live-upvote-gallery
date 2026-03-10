4a833118fd4681ad8f669478d771762999ee9930
Darshik <darshikladhe182@gmail.com>
2026-03-10 11:18:00 +0530

    Started SocketIO

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./routes/postRoutes.js";
 import dotenv from "dotenv";
 import { dbConnect } from "./db/dbConnection.js";
+
 dotenv.config();
 
 const galleryApp = express();
@@ -15,3 +16,19 @@ galleryApp.use("/api", router);
 galleryApp.listen(port, () =>
   console.log(`Server is up for you ;) at ${port}`),
 );
+
+/* Socket IO Backend Stuff */
+
+import { Server } from "socket.io";
+import http from "http";
+
+const ser = http.createServer(galleryApp);
+const ioApp = new Server(ser);
+
+ioApp.on("connection", (socket) => {
+  console.log("User connected to the server");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected from the server");
+  });
+});
