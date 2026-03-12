da9228e826f688ac973aadac2d3e1f47585ef0e4
Darshik <darshikladhe182@gmail.com>
2026-03-12 17:14:12 +0530

    Setup and tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>APP</h1>
+      <h2 className="text-9xl font-bold">Frontend Task 3</h2>
+    </div>
+  );
+};
+
+export default App;
