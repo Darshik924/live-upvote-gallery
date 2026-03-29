7d21dab7fcb9a29cb4dcf5287b9e380727c5f1dd
Darshik <darshikladhe182@gmail.com>
2026-03-29 11:17:40 +0530

    Added Basic Gallery

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import PostForm from "./components/PostForm";
 import Navbar from "./components/Navbar";
+import Gallery from "./components/Gallery";
 
 const App = () => {
   return (
     <div>
       {/* <PostForm/> */}
       <Navbar />
+      <Gallery />
     </div>
   );
 };
--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import ImageCard from "./ImageCard";
+// import ImageCard from "./ImageCard";
 
 const Gallery = () => {
   const Arr = [1, 2, 3, 4, 45, 6];
 
   return (
-    <main className="flex flex-wrap gap-10 pt-17 min-h-screen">
-      {Arr.map((ele) => (
-        <ImageCard key={ele} />
-      ))}
+    <main className="min-h-screen bg-gray-100 pt-20 px-6">
+      {/* Heading */}
+      <h1 className="text-3xl font-bold text-gray-800 mb-6">Image Gallery</h1>
+
+      {/* Grid Container */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {Arr.map((ele) => (
+          <ImageCard key={ele} />
+        ))}
+      </div>
     </main>
   );
 };
